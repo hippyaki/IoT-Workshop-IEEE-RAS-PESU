592173d71b2e8e0d5c4017ebdb2e8cb0bc5caf79
Akshayan Sinha <akshayan.sinha@gmail.com>
2021-10-29 22:53:02 +0530

    added AccessPoint example

---
+++ WebServer on ESP32/ESP32_Webserver_AP/ESP32_Webserver_AP.ino
@@ -0,0 +1,101 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <Wire.h>
+
+#include "SparkFunBME280.h" 
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+BME280 bme;
+
+float temperature, humidity, pressure, altitude;
+
+/*Put your SSID & Password*/
+const char* ssid = "<wifi_name>";  // Enter SSID here
+const char* password = "<password>";  //Enter Password here (min. 8 characters)
+
+/* Put IP Address details */
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+WebServer server(80);          
+ 
+void setup() {
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Setting AP (Access Point)…");
+  
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  WiFi.softAP(ssid, password);
+  
+  
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+  //--------------------------------------------------------
+  
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+  
+  server.begin();
+  Serial.println("HTTP server started");
+  Wire.begin();
+
+  if (bme.beginI2C() == false) //Begin communication over I2C
+  {
+    Serial.println("The sensor did not respond. Please check wiring.");
+    while(1); //Freeze
+  }
+}
+void loop() {
+  server.handleClient();
+}
+
+void handle_OnConnect() {
+  temperature = bme.readTempC();
+  humidity = bme.readFloatHumidity();
+  pressure = bme.readFloatPressure() / 100.0F;
+  altitude = bme.readFloatAltitudeFeet();
+  server.send(200, "text/html", SendHTML(temperature,humidity,pressure,altitude)); 
+  Serial.print(temperature);
+  Serial.print(humidity);
+  Serial.print(pressure);
+  Serial.println(altitude);
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(float temperature,float humidity,float pressure,float altitude){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>ESP32 Weather Station</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<div id=\"webpage\">\n";
+  ptr +="<h1>ESP32 Weather Station</h1>\n";
+  ptr +="<p>Temperature: ";
+  ptr +=temperature;
+  ptr +="&deg;C</p>";
+  ptr +="<p>Humidity: ";
+  ptr +=humidity;
+  ptr +="%</p>";
+  ptr +="<p>Pressure: ";
+  ptr +=pressure;
+  ptr +="hPa</p>";
+  ptr +="<p>Altitude: ";
+  ptr +=altitude;
+  ptr +="m</p>";
+  ptr +="</div>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
